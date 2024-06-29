1b1a5dbc37e3e871e4a6236c67281ffcc3ca8cb4
emmanuel ronoh <eronoh036@gmail.com>
2024-06-29 15:48:55 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } 
+  if (feet <= 400 && feet >2000) {
+    return "I will gladly take your twenty bucks."
+  }
+  else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+   else {
+    return "No can do."
+  }
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  }
+
+
+  function switchOnCharmFromTip(tip) {
+    if (tip === "generous") {
+      return "Thank you so much.";
+    } else if (tip === "not as generous") {
+      return "Thank you.";
+    } else {
+      return "Bye.";
+    }
+  }
+
